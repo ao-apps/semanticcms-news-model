70175165ef75848e3c42e9482a87f6deca272b55
AO Industries, Inc <support@aoindustries.com>
2021-06-05 01:49:59 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/semanticcms/news/model/News.java
+++ src/main/java/com/semanticcms/news/model/News.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-model - SemanticCMS newsfeeds.
- * Copyright (C) 2016, 2017, 2020  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,7 +22,7 @@
  */
 package com.semanticcms.news.model;
 
-import static com.aoindustries.lang.Strings.nullIfEmpty;
+import static com.aoapps.lang.Strings.nullIfEmpty;
 import com.semanticcms.core.model.Element;
 import org.joda.time.DateTime;
 import org.joda.time.ReadableDateTime;
